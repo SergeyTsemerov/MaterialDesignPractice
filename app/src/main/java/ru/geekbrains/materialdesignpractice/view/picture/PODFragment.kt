718b8260439e82d1234d7952d492718c15d125d3
SergeyTsemerov <sergeytsemerov@gmail.com>
2021-09-16 21:07:45 +0300

    Lesson 2

--- app/src/main/java/ru/geekbrains/materialdesignpractice/view/picture/PODFragment.kt
+++ app/src/main/java/ru/geekbrains/materialdesignpractice/view/picture/PODFragment.kt
@@ -3,24 +3,22 @@ package ru.geekbrains.materialdesignpractice.view.picture
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import coil.load
-import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomappbar.BottomAppBar
 import ru.geekbrains.materialdesignpractice.R
 import ru.geekbrains.materialdesignpractice.databinding.FragmentMainBinding
+import ru.geekbrains.materialdesignpractice.view.MainActivity
+import ru.geekbrains.materialdesignpractice.view.settings.SettingsFragment
 import ru.geekbrains.materialdesignpractice.view.showSnackBar
-import ru.geekbrains.materialdesignpractice.view.showToast
+import ru.geekbrains.materialdesignpractice.view.showToastLong
 import ru.geekbrains.materialdesignpractice.viewmodel.PODData
 import ru.geekbrains.materialdesignpractice.viewmodel.PODViewModel
 
 class PODFragment : Fragment() {
-
-    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
@@ -35,7 +33,45 @@ class PODFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMainBinding.inflate(inflater)
+        setActionBar()
         return binding.root
+    }
+
+    private var isMain = true
+
+    private fun setActionBar() {
+        (context as MainActivity).setSupportActionBar(binding.bottomAppBar)
+        setHasOptionsMenu(true)
+        binding.fab.setOnClickListener {
+            if (isMain) {
+                isMain = false
+                binding.bottomAppBar.navigationIcon = null
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+                binding.fab.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_back_fab
+                    )
+                )
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar_other_screen)
+            } else {
+                isMain = true
+                binding.bottomAppBar.navigationIcon =
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_hamburger_menu_bottom_bar
+                    )
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+                binding.fab.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_plus_fab
+                    )
+                )
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar)
+            }
+        }
+
     }
 
     override fun onDestroyView() {
@@ -49,13 +85,11 @@ class PODFragment : Fragment() {
         viewModel.sendServerRequest()
         binding.inputLayout.setEndIconOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW).apply {
-                data = Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
+                data =
+                    Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
             }
             startActivity(intent)
         }
-
-        bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheetInclude.bottomSheetContainer)
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_DRAGGING
     }
 
     private fun renderData(data: PODData) {
@@ -64,18 +98,44 @@ class PODFragment : Fragment() {
                 binding.main.showSnackBar(getString(R.string.Error))
             }
             is PODData.Loading -> {
-                binding.main.showToast(getString(R.string.Loading))
+                binding.imageView.load(R.drawable.progress_animation) {
+                    error(R.drawable.ic_load_error_vector)
+                }
             }
             is PODData.Success -> {
                 binding.imageView.load(data.serverResponseData.url) {
+                    placeholder(R.drawable.progress_animation)
                     error(R.drawable.ic_load_error_vector)
                 }
-                binding.bottomSheetInclude.textView.text = data.serverResponseData.explanation
+                binding.textView.text = data.serverResponseData.explanation
             }
         }
     }
 
     companion object {
         fun newInstance() = PODFragment()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.appBarDescription -> {
+                binding.main.showToastLong(binding.textView.text.toString())
+            }
+            R.id.appBarSettings -> {
+                requireActivity().supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, SettingsFragment.newInstance())
+                    .addToBackStack("")
+                    .commit()
+            }
+            android.R.id.home -> {
+                BottomNavigationDrawerFragment().show(requireActivity().supportFragmentManager, "")
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
